c26c29d75bc5484bae915156676da44f393362d4
DarioDaF <dario.fagotto@gmail.com>
2022-10-25 00:33:16 +0200

    Initial Release

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using WindowTitler;
+
+// See https://aka.ms/new-console-template for more information
+
+if (args.Length != 1)
+{
+    Console.Error.WriteLine($"Usage: WindowTitler \"old window title\"");
+    return 1;
+}
+
+var titlePrefix = args[0];
+
+var wList = new List<Tuple<IntPtr, string>>();
+
+WinStuff.EnumWindows(delegate (IntPtr hWnd, int lParam) {
+    var title = WinStuff.GetWindowText(hWnd);
+
+    if (title != null && title.StartsWith(titlePrefix))
+    {
+        wList.Add(Tuple.Create(hWnd, title));
+    }
+
+    return true;
+}, 0);
+
+int i = 0;
+foreach(var (w, title) in wList)
+{
+    var newTitle = titlePrefix + " - " + i;
+    
+    Console.WriteLine($"{title} -> {newTitle}");
+
+    WinStuff.SetWindowText(w, newTitle);
+    i += 1;
+}
+
+Console.ReadLine();
+
+return 0;
---
+++ WinStuff.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WindowTitler
+{
+    internal class WinStuff
+    {
+
+        public delegate bool EnumWindowsProc(IntPtr hWnd, int lParam);
+
+        [DllImport("USER32.DLL")]
+        public static extern IntPtr FindWindow(string? className, string windowName);
+
+        [DllImport("USER32.DLL")]
+        public static extern bool SetWindowText(IntPtr hwnd, string windowName);
+
+        [DllImport("USER32.DLL")]
+        public static extern bool EnumWindows(EnumWindowsProc enumFunc, int lParam);
+
+        [DllImport("USER32.DLL")]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("USER32.DLL")]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        public static string? GetWindowText(IntPtr hWnd)
+        {
+            int len = GetWindowTextLength(hWnd);
+            if (len <= 0)
+            {
+                return null;
+            }
+            StringBuilder sb = new StringBuilder(len);
+            GetWindowText(hWnd, sb, len + 1);
+            return sb.ToString();
+        }
+
+        [DllImport("USER32.DLL")]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("USER32.DLL")]
+        public static extern IntPtr GetShellWindow();
+
+    }
+}
